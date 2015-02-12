74d43bbf368f7b6fc750ae324e13e3b2c7ab5479
Fred Yankowski <fcy@imsa.edu>
2015-02-12 17:27:59 -0600

    Create /contacts service path to list all active personnel.

--- hr.go
+++ hr.go
@@ -33,3 +33,47 @@ func PersonnelCount() (int, error) {
 	}
 	return count, nil
 }
+
+type Contact struct {
+	Empno string
+	Fname string
+	Mi string
+	Lname string
+	Active string
+	Jobtitle string
+}
+
+// GetContacts reads from the HR database and returns a channel of Contact structs
+func GetContacts() (chan struct { Contact; Error error }, error) {
+	cs := make(chan struct { Contact; Error error })
+	dsn := os.Getenv("HR_DSN")
+	db, err := sql.Open("mssql", dsn)
+	if err != nil {
+		log.Printf("ERROR: cannot open DSN = %v", dsn)
+		return cs, err
+	}
+	defer db.Close()
+
+	q := `select p_empno, p_active, p_fname, p_mi, p_lname
+ , p_jobtitle
+ from hrpersnl
+ where p_active = 'A' order by p_lname, p_fname`
+	rows, err := db.Query(q)
+	if err != nil {
+		log.Printf("ERROR: query failed")
+		return cs, err
+	}
+
+	go func() {
+		defer rows.Close()
+		defer close(cs)
+		
+		for rows.Next() {
+			c := Contact{}
+			err := rows.Scan(&c.Empno, &c.Active, &c.Fname, &c.Mi, &c.Lname,
+				&c.Jobtitle)
+			cs <- struct { Contact; Error error }{ c, err }
+		}
+	}()
+	return cs, nil
+}
--- service/main.go
+++ service/main.go
@@ -10,13 +10,35 @@ import (
 
 var address = flag.String("address", ":8080", "Listen and serve at this address")
 
-func contacthandler(w http.ResponseWriter, r *http.Request) {
+func counthandler(w http.ResponseWriter, r *http.Request) {
 	c, _ := hrfacade.PersonnelCount()
 	fmt.Fprintf(w, "%d", c)
 }
 
+// contacthandler displays a line for each contact
+func contacthandler(w http.ResponseWriter, r *http.Request) {
+	cs, err := hrfacade.GetContacts()
+	if err != nil {
+		log.Printf("ERROR: GetContacts: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	for ce := range cs {
+		if ce.Error != nil {
+			log.Printf("ERROR: ce.Error = %v", ce.Error)
+			http.Error(w, ce.Error.Error(), http.StatusInternalServerError)
+			return
+		} else {
+			c := ce.Contact
+			fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\t%s\n", c.Empno, c.Active, c.Fname, c.Mi, c.Lname,
+				c.Jobtitle)
+		}
+	}
+}
+
 func main() {
 	flag.Parse()
+	http.HandleFunc("/count", counthandler)
 	http.HandleFunc("/contacts", contacthandler)
 	log.Printf("Listening at %v", *address)
 	log.Fatal(http.ListenAndServe(*address, nil))
